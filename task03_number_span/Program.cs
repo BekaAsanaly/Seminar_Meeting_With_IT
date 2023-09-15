269f1703c63228e7e3bdb23e39edaa0367d3beeb
beka <beqool.3@gmail.com>
2023-09-16 01:31:46 +0600

    task03_number_span

---
+++ task03_number_span/Program.cs
@@ -0,0 +1,31 @@
+﻿/* git push origin main
+Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+2 -> " -2, -1, 0, 1, 2"
+*/
+
+Console.WriteLine("Enter the number");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int a = number;
+
+Console.Write(-a);
+Console.Write(" ");
+while(a > -a)
+
+{
+    Console.Write(-a + 1);
+    Console.Write(" ");
+    a = a - 1;
+    
+}
+
+int b = 0;
+
+while(b < number)
+{
+    Console.Write(b+1);
+    Console.Write(" ");
+    b = b + 1;
+}
+
