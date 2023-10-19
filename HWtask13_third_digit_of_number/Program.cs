27bdc1290d99a23355473429f22cff4df1b8464e
beka <beqool.3@gmail.com>
2023-10-19 20:44:56 +0600

    HWtask13_third_digit_of_number

---
+++ HWtask13_third_digit_of_number/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.WriteLine("Enter the number");
+int number = Convert.ToInt32(Console.ReadLine());
+int threeDigitNumber = 99;
+if (number > 99)
+{
+    if (number > 99 && number < 999)
+    {
+        threeDigitNumber = number % 10;
+    }
+    
+    while (number > 999)
+    {
+        number = number / 10;
+        threeDigitNumber = number % 10;
+    }
+    System.Console.WriteLine(threeDigitNumber);
+}
+
+else
+{
+    Console.WriteLine("there is no third digit");
+}
