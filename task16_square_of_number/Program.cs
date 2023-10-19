4deecf5e3e3154dd6a2ba24e26ef9ce5b7a30293
beka <beqool.3@gmail.com>
2023-10-19 15:39:31 +0600

    task16_square_of_number

---
+++ task16_square_of_number/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого
+5, 25 -> да
+-4, 16 -> да
+25, 5 -> да
+8,9 -> нет5 
+*/
+
+Console.WriteLine("Enter the number1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the number2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 * number1 == number2 || number2 * number2 == number1)
+{
+    Console.WriteLine("one number is the square of the another number");
+}
+
+else 
+{
+    Console.WriteLine("one number isn't the square of the another number");
+}
