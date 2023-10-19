3fd1ec76391a7be8df824c58e2ad710cb3244d2e
beka <beqool.3@gmail.com>
2023-10-19 13:34:56 +0600

    task11_delete_second_number

---
+++ task11_delete_second_number/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+
+int randomThreeDigitNumber = new Random().Next(100, 1000);
+Console.WriteLine(randomThreeDigitNumber);
+
+int number1 = randomThreeDigitNumber / 100;
+int number2 = randomThreeDigitNumber % 10;
+
+int TwoDigitNumber = number1 * 10 + number2;
+Console.WriteLine(TwoDigitNumber);
