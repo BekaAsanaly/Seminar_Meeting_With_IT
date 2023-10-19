23301296f9d54b6d31cc9c0be0a4031319d670c7
beka <beqool.3@gmail.com>
2023-10-19 18:49:59 +0600

    HWtask10_second_of_threedigit_number

---
+++ HWtask10_second_of_threedigit_number/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Enter the three digit number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    int remainder1 = number % 10;
+    int TwoDigitNumber = (number - remainder1) / 10;
+    int resultRemainder = TwoDigitNumber % 10;
+    Console.WriteLine($"second digit of this number - {resultRemainder}");
+}
+else 
+{
+    Console.WriteLine(" Error. Enter the three digit number");
+}
+
+
+
