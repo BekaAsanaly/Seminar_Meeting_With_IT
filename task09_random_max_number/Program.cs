f1933f23ade8f30f03bc90d20cb6e5b7b0bd2f9c
beka <beqool.3@gmail.com>
2023-10-19 13:04:25 +0600

    task09_random_max_number

---
+++ task09_random_max_number/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу, которая выводит случайное число из отрезка [10, 99]  и показывает наибольшую цифру числа.
+78 -> 8
+12 -> 2
+85 -> 8
+*/
+
+int randomNumber = new Random().Next(10, 100);
+Console.WriteLine (randomNumber);
+
+int number1 = randomNumber / 10;
+int number2 = randomNumber % 10;
+
+int max = number1;
+if (number2 > number1) max = number2;
+
+Console.WriteLine(max);
