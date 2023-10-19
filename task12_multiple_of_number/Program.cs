37b2daeecd94401cbaba01e6b0f6dfcce3eddc22
beka <beqool.3@gmail.com>
2023-10-19 14:42:39 +0600

    task12_multiple_of_number

---
+++ task12_multiple_of_number/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Напишите программу, которая будет принимать на вход два числа и выводить, 
+является ли второе число кратным первому. Если второе число некратно первому, 
+то программа выводит остаток от деления.
+34, 5 -> некратно, остаток 4
+16, 4 -> кратно
+*/
+
+Console.WriteLine("Enter the first number");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second number");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int divisionReminder = number1 % number2;
+
+if (number1 % number2 == 0)
+{
+    Console.WriteLine("the second number is a multiple of the first number");
+}
+
+else 
+{
+    Console.WriteLine("Division reminder is" + " " + "-" + " " + divisionReminder);
+}
+
+
